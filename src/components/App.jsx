d9b5a4db50cc82bd9c2c2f776301662a370d4a23
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-21 17:33:10 +0200

    create simple routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="dashboard" element={<div>dashboard</div>} />
+          <Route path="sales" element={<div>sales</div>} />
+          <Route path="reports" element={<div>reports</div>} />
+          <Route path="feedback" element={<div>feedback</div>} />
+          <Route path="customers" element={<div>customers</div>} />
+        </Route>
+      </Routes>
+    </>
   );
 };
