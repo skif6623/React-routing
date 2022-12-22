b1f323c682428df725ac1234355208a928004b28
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-22 19:00:29 +0200

    added index routes and sharedLayout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,31 +3,16 @@ import { About } from '../pages/About';
 import { Home } from '../pages/Home';
 import { ProductDetails } from '../pages/ProductDetails';
 import { Products } from '../pages/Products';
-import { Container, Header, Logo, Link } from './App.styled';
 import { Mission } from './Mission';
 import { Reviews } from './Reviews';
 import { Team } from './Team';
+import { SharedLayout } from './SharedLayout';
 
 export const App = () => {
   return (
-    <Container>
-      <Header>
-        <Logo>
-          <span role="img" aria-label="computer icon">
-            💻
-          </span>{' '}
-          GoMerch Store
-        </Logo>
-        <nav>
-          <Link to="/" end>
-            Home
-          </Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-      </Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
         <Route path="/about" element={<About />}>
           <Route path="mission" element={<Mission />} />
           <Route path="team" element={<Team />} />
@@ -35,7 +20,7 @@ export const App = () => {
         </Route>
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetails />} />
-      </Routes>
-    </Container>
+      </Route>
+    </Routes>
   );
 };
