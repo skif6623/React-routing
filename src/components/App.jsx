92f5b5af996a7458795ffa2a1c23915ada197c3b
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-22 16:28:21 +0200

    added navlink and styled it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-
+import { StyledLink } from './App.styled';
 export const App = () => {
-  return <></>;
+  return (
+    <div>
+      <nav>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="about" element={<div>About</div>} />
+        <Route path="products" element={<div>Products</div>} />
+        <Route path="*" element={<div>Not found</div>} />
+      </Routes>
+    </div>
+  );
 };
---
+++ src/components/App.styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const StyledLink = styled(NavLink)`
+  display: block;
+  color: black;
+  font-size: 20px;
+  background-color: tomato;
+  text-decoration: none;
+  padding: 10px 15px;
+
+  &.active {
+    background-color: red;
+    color: white;
+  }
+
+  :hover:not(.active) {
+    background-color: red;
+    color: white;
+  }
+`;
