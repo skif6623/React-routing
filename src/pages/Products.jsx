7751de2e0e7642fd648b366168869c958fbbaa9c
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-22 22:37:50 +0200

    created Search component and added it into Products component

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,23 @@
+import { useSearchParams } from 'react-router-dom';
+import { SearchBox } from 'components/SearchBox';
 import { ProductList } from '../components/ProductList';
 import { getProducts } from '../fakeAPI';
 
 export const Products = () => {
   const products = getProducts();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const productName = searchParams.get('name') ?? '';
+  const visibleProducts = products.filter(product =>
+    product.name.toLowerCase().includes(productName.toLowerCase())
+  );
+
+  const updateQuerryString = name => {
+    const nextParams = name !== '' ? { name } : {};
+  };
   return (
     <main>
-      <ProductList products={products} />
+      <SearchBox value={productName} onChange={updateQuerryString} />
+      <ProductList products={visibleProducts} />
     </main>
   );
 };
