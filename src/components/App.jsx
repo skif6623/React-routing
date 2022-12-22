c330083a308c7ee49e41dd1b96204054fdbdec64
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-22 16:07:06 +0200

    added basename and started pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
+
 export const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="dashboard" element={<div>dashboard</div>} />
-          <Route path="sales" element={<div>sales</div>} />
-          <Route path="reports" element={<div>reports</div>} />
-          <Route path="feedback" element={<div>feedback</div>} />
-          <Route path="customers" element={<div>customers</div>} />
-        </Route>
-      </Routes>
-    </>
-  );
+  return <></>;
 };
